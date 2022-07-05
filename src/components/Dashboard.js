7a8dba6c4af32d198fedc71c0d88ef4adcc8b0d1
Josh Smith <josh@jshsmth.com>
2022-07-05 14:31:40 +1000

    basic choose user function extended to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,29 +4,31 @@ import Typography from "@mui/material/Typography";
 import Nav from "./Nav";
 import { useSelector, useDispatch } from "react-redux";
 import DashboardCard from "./DashboardCard";
+import { CollectionsOutlined } from "@mui/icons-material";
 
 function Dashboard({ allQuestions }) {
   const currentUser = useSelector((state) => state.users.userLoggedIn);
   const userQuestions = allQuestions[0] || {};
-  const userQuestionsIds = Object.keys(userQuestions);
+
+  console.log(userQuestions);
+
   // const users = useSelector((state) => state.users);
   const dispatch = useDispatch();
-  const questionsAnswered = currentUser?.answers;
-  const questions = currentUser?.questions;
+  const currentUserAnswers = currentUser?.answers;
+  const currentUserQuestions = currentUser?.questions;
 
-  // console.log(userQuestions);
-  console.log(userQuestionsIds);
+  console.log(currentUserAnswers);
 
   return (
     <Box>
       <Nav />
       <h1>Welcome, {currentUser?.name}</h1>
       <Typography variant="h5" component="div">
-        New Questions
+        Unanswered
       </Typography>
       <DashboardCard title="New questions" />
       <Typography variant="h5" component="div">
-        Done
+        Answered
       </Typography>
       <DashboardCard title="Done" />
     </Box>
