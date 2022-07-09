e6236a0e94b6ecd78a3998f270453b300e52e4b1
Josh Smith <josh@jshsmth.com>
2022-07-09 16:40:14 +1000

    test progress

---
+++ src/components/tests/ViewPoll.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import ViewPoll from "../ViewPoll";
+
+describe("ViewPoll", () => {
+  it("Should be able to create poll", () => {
+    /////////
+  });
+  it("Should match snapshot", () => {
+    expect(ViewPoll).toMatchSnapshot();
+  });
+});
