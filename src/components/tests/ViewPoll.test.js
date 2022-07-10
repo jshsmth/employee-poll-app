7e13ee9e6ed9386c13aae171097fb9271a99e1ee
Josh Smith <josh@jshsmth.com>
2022-07-10 18:41:25 +1000

    created test helper

--- src/components/tests/ViewPoll.test.js
+++ src/components/tests/ViewPoll.test.js
@@ -1,9 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import ViewPoll from "../ViewPoll";
+import { reduxWrapper } from "../tests/testHelpers";
 
 describe("ViewPoll", () => {
   it("Should be able to create poll", () => {
-    /////////
+    reduxWrapper(<ViewPoll />);
   });
   it("Should match snapshot", () => {
     expect(ViewPoll).toMatchSnapshot();
