317171b1d083fefd80df313e1ca0afc07d516945
Josh Smith <josh@jshsmth.com>
2022-07-10 20:53:16 +1000

    changed to memory router

--- src/components/tests/testHelpers.js
+++ src/components/tests/testHelpers.js
@@ -1,11 +1,11 @@
 import { render } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 export function reduxWrapper(component, store) {
   return render(
     <Provider store={store}>
-      <BrowserRouter>{component}</BrowserRouter>
+      <MemoryRouter>{component}</MemoryRouter>
     </Provider>
   );
 }
