276d47191e0d98554bf3857e66c347fad1770532
Josh Smith <josh@jshsmth.com>
2022-07-11 12:29:27 +1000

    fix: notfound page now requreis user to be selected

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Nav from "./Nav";
+import App from "./App";
 
 function NotFound() {
+  const userLoggedIn = useSelector((state) => state.users?.userLoggedIn?.id);
   return (
     <div>
-      <h1>
-        Sorry mate something has happened to the page we were trying to find or
-        we need to authiencate you again for the current action you are
-        performing{" "}
-      </h1>
-      <Link to="/">Go back to home or login</Link>
+      {userLoggedIn && (
+        <>
+          {userLoggedIn && <Nav />}
+          <h1>
+            Sorry mate something has happened to the page we were trying to find
+            or we need to authiencate you again for the current action you are
+            performing{" "}
+          </h1>
+          <Link to="/">Go back to dashboard</Link>
+        </>
+      )}
+      {!userLoggedIn && <App />}
     </div>
   );
 }
